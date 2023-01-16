8ebf4bfb14db32cdda5959ca062b763f3a9cff00
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-16 16:10:47 +0100

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'date'
+
+
+def generateUser(n)
+    n.times do 
+        firstName = Faker::Name.first_name
+        lastName = Faker::Name.last_name
+        User.create(
+            email: "#{firstName}.#{lastName}@yopmail.com",
+            encrypted_password: "424242", 
+            description: Faker::Lorem.sentence, 
+            first_name: firstName, 
+            last_name: lastName
+        )
+    end
+end
+generateUser(5)
+
+Event.create(
+    start_date: DateTime.new(2001,rand(1...12),rand(1...28),4,5,6),
+    #=> #<DateTime: 2001-02-03T04:05:06+00:00 ...>
+    duration: rand(1...90),
+    title: "First Event",
+    description: Faker::Lorem.paragraph,
+    price: rand(1...1000),
+    location: "Paris"
+)
+
+Attendance.create(
+    stripe_customer_id: "1876928D7"
+)
