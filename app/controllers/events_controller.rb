b000d15a9f3eba0ca8732956d72f459b7a854316
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-17 12:04:01 +0100

    create event debug + bootstrap

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -12,16 +12,14 @@ class EventsController < ApplicationController
     @event = Event.new
   end
   def create
-    Event.create(
-      start_date: DateTime.new(2001,rand(1...12),rand(1...28),4,5,6),
-      #=> #<DateTime: 2001-02-03T04:05:06+00:00 ...>
-      duration: rand(1...90),
-      title: Faker::Lorem.word,
-      description: Faker::Lorem.paragraph,
-      price: rand(1...1000),
-      location: "Paris",
-      user: current_user
-  )
-  redirect_to root_path
+    @event = Event.create(event_params)
+    @event.user = current_user
+    @event.save
+    redirect_to root_path
   end
+
+  private
+    def event_params
+        params.require(:event).permit(:start_date, :duration, :title, :description, :price, :location, :user)
+    end
 end
